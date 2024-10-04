341d7885e8575ade8eb2918662f8b183fe9f8825
Eto0601 <h150601koki@outlook.jp>
2024-10-04 12:18:53 +0900

    CRUD機能の実装

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -9,4 +9,60 @@ class Player extends Model
 {
     public $timestamps = false;
     use HasFactory;
+
+   /**
+     * プレイヤーを全件取得する
+     * 
+     * @return 全プレイヤー情報
+     */
+    public function playerIndex() 
+    {
+        return (Player::query()->get());
+    }
+
+    /**
+     * プレイヤーを1件取得
+     * @return １件のプレイヤー情報
+     */
+    public function playerShow($id) 
+    {
+        return (Player::query()->where('id', $id)->select('id' ,'name')->first());
+    }
+
+    
+    /**
+     * 新規プレイヤーのレコードを作成し、idを返す
+     * 
+     * @param int name,hp,mp,money
+     * @return 新規プレイヤーのid
+     */
+    public function playerCreate($name, $hp, $mp, $money)
+    {  
+        return(Player::query()
+        ->insertGetId(
+            ['name'=>$name,
+            'hp'=>$hp,
+            'mp'=>$mp,
+            'money'=>$money]
+            )
+        );
+    }
+   
+    public function playerUpdate($id,$name,$hp,$mp,$money)
+    {
+        return(Player::query() 
+        ->where('id', $id)
+        ->update(['name'=>$name,
+            'hp'=>$hp,
+            'mp'=>$mp,
+            'money'=>$money]
+             )
+        );
+    }
+
+    public function playerDestroy($id)
+    {
+        return(Player::query() ->where('id',$id)
+        ->delete());
+    }
 }
